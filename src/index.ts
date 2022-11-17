167aaa5436e084ad43f6e65d305a3f76931cb36b
devofficer <myeyesontech@gmail.com>
2022-11-17 14:13:55 -0800

    [feat] implemented finnhub api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import bodyParser from 'body-parser';
+import axios from "axios";
 
 dotenv.config();
 
@@ -10,11 +12,22 @@ if (!process.env.PORT) {
 }
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
+const FINNHUB_URL: string = process.env.FINNHUB_URL as string;
+const FINNHUB_KEY: string = process.env.FINNHUB_KEY as string;
 const app = express();
+
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/stock/:ticker', async (req, res) => {
+    const { ticker } = req.params;
+    const url = `${FINNHUB_URL}/quote`;
+    const result = await axios.get(url, {params: { symbol: ticker, token: FINNHUB_KEY }});
+    res.json(result.data);
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
